d1440b1407d903c4a3fd6dfdfd8a44025fa90d02
Carlos Escriche Izquierdo <escrichov@gmail.com>
2014-01-22 17:49:34 +0100

    HFAS

---
+++ Guide/src/com/guide/db/PlaceAdapter.java
@@ -0,0 +1,17 @@
+package com.guide.db;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class PlaceAdapter {
+
+	private static class ViewHolder
+	{
+		TextView title;
+		TextView category;
+		TextView desctiption;
+		TextView rating;
+		ImageView imageCategory;
+	}
+	
+}
